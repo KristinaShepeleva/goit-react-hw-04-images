32453e17e3d1d59c8f04342422bc22348c80c1f0
KristinaShepeleva <shepelevakristina52@gmail.com>
2023-05-13 17:13:59 +0300

    fix

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -42,39 +42,3 @@ Modal.propTypes = {
     children: PropTypes.node.isRequired,
     onClose: PropTypes.func.isRequired,
 };
-
-
-// export function Modal({ onCloseModal, largeImage, tags }) {
-//   useEffect(() => {
-//     function handleCloseECC(e) {
-//       if (e.key === 'Escape') {
-//         onCloseModal();
-//       }
-//     }
-
-//     window.addEventListener('keydown', handleCloseECC);
-
-//     return () => window.removeEventListener('keydown', handleCloseECC);
-//   }, [onCloseModal]);
-
-//   function handleCloseBackdrop(e) {
-//     if (e.currentTarget === e.target) {
-//       onCloseModal();
-//     }
-//   }
-
-//   return createPortal(
-//     <div className="Overlay" onClick={handleCloseBackdrop}>
-//       <div className="Modal">
-//         <img src={largeImage} alt={tags} width="1000" />
-//       </div>
-//     </div>,
-//     modalRoot
-//   );
-// }
-
-// Modal.propTypes = {
-//   onCloseModal: PropTypes.func.isRequired,
-//   largeImage: PropTypes.string.isRequired,
-//   tags: PropTypes.string.isRequired,
-// };
